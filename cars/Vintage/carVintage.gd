7c29e38d0eeaf6d7e5fda2ef795166e7f642f332
fvrbic <filip.vrbic@fer.hr>
2023-12-16 18:39:39 +0100

    izmijenjen wheel_roll_influence na VintageCar, dodan manualni mjenjač

--- cars/Vintage/carVintage.gd
+++ cars/Vintage/carVintage.gd
@@ -8,7 +8,7 @@ extends VehicleBody3D
 @export var STEER_SPEED = 1.5
 @export var STEER_LIMIT = 0.8
 var steer_target = 0
-@export var engine_force_value = 40
+@export var engine_force_value = 400
 @export var steering_wheel_speed = 10
 
 @export var SENSITIVITY = 0.015
@@ -23,6 +23,7 @@ var steer_target = 0
 @export var rotating_camera_limit_top := 60
 @onready var floating_camera = $look/floatingCamera
 @onready var speed_text = %speed
+@onready var gear_text = %gear
 
 @onready var left_camera = $Hud/Container/LeftMirror/Subviewport/Node3D
 @onready var right_camera = $Hud/Container/RightMirror/Subviewport/Node3D
@@ -30,6 +31,8 @@ var steer_target = 0
 @onready var middle_mirror_marker = $MiddleMirrorMarker
 @onready var right_mirror_marker = $RightMirrorMarker
 @onready var left_mirror_marker = $LeftMirrorMarker
+
+var gear = 1
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -77,40 +80,64 @@ func _process(delta):
 
 func _physics_process(delta):
 	var speed = linear_velocity.length()*Engine.get_frames_per_second()*delta
+	var kmph = speed*3.8
 	traction(speed)
-	speed_text.text=str(round(speed*3.8))+"  KMPH"
+	speed_text.text=str(round(kmph))+"  KMPH"
+	gear_text.text= str(gear)
 
-	var fwd_mps = transform.basis.x.x
+
 	steer_target = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
 	steer_target *= STEER_LIMIT
 	if Input.is_action_pressed("ui_up"):
 	# Increase engine force at low speeds to make the initial acceleration faster.
 
-		if speed < 20 and speed != 0:
-			engine_force = clamp(engine_force_value * 3 / speed, 0, 300)
-		else:
-			engine_force = engine_force_value
+		if (gear == 1):
+			if (kmph >= 25):
+				engine_force = 0
+			else:
+				engine_force = engine_force_value
+		elif (gear == 2):
+			if (kmph >= 40):
+				engine_force = 0
+			else:
+				engine_force = engine_force_value * 0.8
+		elif (gear == 3):
+			if (kmph >= 70):
+				engine_force = 0
+			else:
+				engine_force = engine_force_value * 0.6
+		elif (gear == 4):
+			if (kmph >= 105):
+				engine_force = 0
+			else:
+				engine_force = engine_force_value * 0.5
+		elif (gear == 5):
+			engine_force = engine_force_value * 0.4	
+	
+	elif Input.is_action_pressed("ui_down"):
+		engine_force = -engine_force_value*0.5
+		
+		
 	else:
 		engine_force = 0
-	if Input.is_action_pressed("ui_down"):
-		# Increase engine force at low speeds to make the initial acceleration faster.
-		if fwd_mps >= -1:
-			if speed < 30 and speed != 0:
-				engine_force = -clamp(engine_force_value * 10 / speed, 0, 300)
-			else:
-				engine_force = -engine_force_value
-		else:
-			brake = 1
-	else:
-		brake = 0.0
+	
+	if Input.is_action_just_pressed("gear_up"):
+		if (gear < 5):
+			gear += 1
 		
+	if Input.is_action_just_pressed("gear_down"):
+		if (gear > 1):
+			gear -= 1
+	
 	if Input.is_action_pressed("ui_select"):
-		brake=3
+		
 		$right_rear.wheel_friction_slip=0.8
 		$left_rear.wheel_friction_slip=0.8
 	else:
 		$right_rear.wheel_friction_slip=1.4
 		$left_rear.wheel_friction_slip=1.4
+	
+	
 	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
 	
 	
